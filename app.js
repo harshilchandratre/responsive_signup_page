520019b4abbd8833a8f081d3d6712c33f6de5df0
harshilchandratre <harshilchandratre2002@gmail.com>
2023-10-17 02:06:52 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// const enteredPassword = document.getElementById('password');
+// const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+// const passwordToggler = document.getElementById('password-toggler'); // Assuming there's only one element with this ID.
+
+// enteredPassword.addEventListener('input', () => {
+//     const passwordValue = enteredPassword.value; // Get the value of the input.
+
+//     if (passwordPattern.test(passwordValue)) {
+//         passwordToggler.style.display = 'block';
+//     } else {
+//         passwordToggler.style.display = 'none';
+//     }
+// });
